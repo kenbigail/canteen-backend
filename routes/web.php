c3c3a283bbd924f2f62a1de1def71db5aaec8416
LeBrontoJames <keenanabigailidn@gmail.com>
2024-10-22 09:56:56 +0700

    auth

---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('pages.auth.login');
+});
+
+Route::get('/home', function () {
+    return view('pages.dashboard');
+});
+
+
